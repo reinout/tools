d9b89b2490ec4f1f51a2ede2af14a438f923cf51
Reinout van Rees <reinout@vanrees.org>
2011-12-29 18:16:41 +0100

    Added local checkout dev packages

---
+++ install_local_checkouts.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+bin/pip install ~/git/zest.releaser/
+bin/pip install ~/git/pep8/
+bin/pip install ~/hg/checkoutmanager
+bin/pip install ~/git/nens/nensskel
+bin/pip install ~/hg/eolfixer
+bin/pip install ~/hg/createcoverage
