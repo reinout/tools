bb622fb9e23278167e7aecfcec58360152433b33
Reinout van Rees <reinout@vanrees.org>
2012-09-22 13:40:25 +0200

    compensating for new vagrant-based dir locations

--- install_local_checkouts.sh
+++ install_local_checkouts.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 bin/pip install -e .
-bin/pip install -e ~/git/zest.releaser/
-bin/pip install -e ~/git/pep8/
-bin/pip install -e ~/hg/checkoutmanager
-bin/pip install -e ~/git/nens/nensskel
-bin/pip install -e ~/hg/eolfixer
-bin/pip install -e ~/hg/createcoverage
+bin/pip install -e ~/vm/django/utils/zest.releaser/
+bin/pip install -e ~/vm/django/utils/pep8/
+bin/pip install -e ~/vm/django/utils/checkoutmanager
+bin/pip install -e ~/vm/django/utils/nens/nensskel
+bin/pip install -e ~/vm/django/utils/eolfixer
+bin/pip install -e ~/vm/django/utils/createcoverage
