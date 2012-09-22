00562ed6f8f46bab656387da56d5cf580e4c6dc4
Reinout van Rees <reinout@vanrees.org>
2012-09-22 13:50:02 +0000

    Handling local checkouts also in the vm

--- install_local_checkouts.sh
+++ install_local_checkouts.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+if [ -d ~/vm/django/utils ]; then
+    ROOT=~/vm/django
+fi
+if [ -d /vagrant/utils ]; then
+    ROOT=/vagrant
+fi
+
 bin/pip install -e .
-bin/pip install -e ~/vm/django/utils/zest.releaser/
-bin/pip install -e ~/vm/django/utils/pep8/
-bin/pip install -e ~/vm/django/utils/checkoutmanager
-bin/pip install -e ~/vm/django/utils/nens/nensskel
-bin/pip install -e ~/vm/django/utils/eolfixer
-bin/pip install -e ~/vm/django/utils/createcoverage
+bin/pip install -e $ROOT/utils/zest.releaser/
+bin/pip install -e $ROOT/utils/pep8/
+bin/pip install -e $ROOT/utils/checkoutmanager
+bin/pip install -e $ROOT/utils/nens/nensskel
+bin/pip install -e $ROOT/utils/eolfixer
+bin/pip install -e $ROOT/utils/createcoverage
