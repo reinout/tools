1592c2dfea0760168556ac573823c2dc1b1883bc
Reinout van Rees <reinout@vanrees.org>
2012-09-24 08:40:08 +0000

    Fixed nensskel path

--- install_local_checkouts.sh
+++ install_local_checkouts.sh
@@ -10,6 +10,6 @@ bin/pip install -e .
 bin/pip install -e $ROOT/utils/zest.releaser/
 bin/pip install -e $ROOT/utils/pep8/
 bin/pip install -e $ROOT/utils/checkoutmanager
-bin/pip install -e $ROOT/utils/nens/nensskel
+bin/pip install -e $ROOT/utils/nensskel
 bin/pip install -e $ROOT/utils/eolfixer
 bin/pip install -e $ROOT/utils/createcoverage
