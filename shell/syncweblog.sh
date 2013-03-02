4a8f83b9336b1f524d9bf518a3dc2062b35d3933
Reinout van Rees <reinout@vanrees.org>
2013-03-02 13:06:50 +0100

    Syncing to the new webserver instead of the old

--- shell/syncweblog.sh
+++ shell/syncweblog.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # Purely personal. rsyncs my local html files with my webserver :-)
 
-rsync -av /Users/reinout/git/reinout.vanrees.org/docs/build/html/ vanrees.org:buildout/reinout.vanrees.org/docs/build/html
 rsync -av /Users/reinout/git/reinout.vanrees.org/docs/build/html/ new.vanrees.org:git/reinout.vanrees.org/docs/build/html
