7692caae9dab32ed0cbf79dd570f10ea499beceb
Reinout van Rees <reinout@vanrees.org>
2013-04-10 10:21:01 +0200

    Added docstring and main() method

--- tools/naw.py
+++ tools/naw.py
@@ -1,6 +1,17 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
+"""
+Company-internal script to query our telephone list.
 
+We have an internal tool that regularly pings laptops to see if somebody's in
+the house. That data, combined with a telephone number list, is available as a
+JSON file.
+
+This script reads the json and prints it, optionally filtered with
+
+(Script is from Arjan Verkerk, not me, btw. Though I modified it a bit to run
+as a setuptools console script.)
+
+"""
 from __future__ import print_function
 from __future__ import unicode_literals
 from __future__ import absolute_import
@@ -11,35 +22,37 @@ import urllib2
 import sys
 import re
 
-if sys.platform.startswith('win'):
-	GREEN = '{}'
-	RED = '{}'
-else:
-	GREEN = '\x1b[1m\x1b[32m{}\x1b(B\x1b[m'
-	RED = '\x1b[1m\x1b[31m{}\x1b(B\x1b[m'
 
-TEMPLATE = '{NAAM:<35}{INTERN:>9}{MOBIEL:>13}  {PRESENCE:<20}'
-
-jsonfile = urllib2.urlopen(
-    'http://buildbot.lizardsystem.nl/gis/aanwezigheid.json',
-)
-data = json.load(jsonfile)
-jsonfile.close()
-
-try:
-    pattern = sys.argv[1]
-except IndexError:
-    pattern = ''
-
-print('   Gericht overnemen: *59 / Prefix interne nummers: 030 2330')
-print(63 * '-')
-
-for elem in data[:-1]:
-    if elem['in_office']:
-        presence = GREEN.format('wel')
+def main():
+    if sys.platform.startswith('win'):
+            GREEN = '{}'
+            RED = '{}'
     else:
-        presence = RED.format('niet')
-    elem.update(PRESENCE=presence)
-    text = TEMPLATE.format(**elem)
-    if re.search(pattern, text, flags=re.IGNORECASE):
-        print(text)
+            GREEN = '\x1b[1m\x1b[32m{}\x1b(B\x1b[m'
+            RED = '\x1b[1m\x1b[31m{}\x1b(B\x1b[m'
+
+    TEMPLATE = '{NAAM:<35}{INTERN:>9}{MOBIEL:>13}  {PRESENCE:<20}'
+
+    jsonfile = urllib2.urlopen(
+        'http://buildbot.lizardsystem.nl/gis/aanwezigheid.json',
+    )
+    data = json.load(jsonfile)
+    jsonfile.close()
+
+    try:
+        pattern = sys.argv[1]
+    except IndexError:
+        pattern = ''
+
+    print('   Gericht overnemen: *59 / Prefix interne nummers: 030 2330')
+    print(63 * '-')
+
+    for elem in data[:-1]:
+        if elem['in_office']:
+            presence = GREEN.format('wel')
+        else:
+            presence = RED.format('niet')
+        elem.update(PRESENCE=presence)
+        text = TEMPLATE.format(**elem)
+        if re.search(pattern, text, flags=re.IGNORECASE):
+            print(text)
