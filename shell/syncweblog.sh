2cb17383afdc47c20e4f1174b1d2a4c8b20796f3
Reinout van Rees <reinout@vanrees.org>
2014-05-14 01:53:07 +0200

    Zapped -v

--- shell/syncweblog.sh
+++ shell/syncweblog.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Purely personal. rsyncs my local html files with my webserver :-)
 
-rsync -av /Users/reinout/git/reinout.vanrees.org/docs/build/html/ new.vanrees.org:git/reinout.vanrees.org/docs/build/html
+rsync -a /Users/reinout/git/reinout.vanrees.org/docs/build/html/ new.vanrees.org:git/reinout.vanrees.org/docs/build/html
