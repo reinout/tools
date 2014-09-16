04136969916ae5380adc7ad4d95db4456a4c870b
Reinout van Rees <reinout@vanrees.org>
2014-09-16 12:35:16 +0200

    New convention: ~/repos for private git repos instead of ~/git

--- shell/create_git_repo.sh
+++ shell/create_git_repo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Initialize a git repository in the temp directory and push it to my own
 # server. I should have created a repository there on the server already with
-# ``git init ~/git/the_project_name --bare``.
+# ``git init ~/repos/the_project_name --bare``.
 
 cd /tmp
 git init $1
@@ -9,5 +9,5 @@ cd $1
 echo "hurray" > README.rst
 git add README.rst
 git commit -m "Added readme"
-git remote add origin ssh://vanrees.org/~/git/$1
+git remote add origin ssh://vanrees.org/~/repos/$1
 git push origin master
