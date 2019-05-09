72fd5cf605a80b6ef7243aad09c5def7b32b3f43
Reinout van Rees <reinout@vanrees.org>
2019-05-09 21:20:50 +0200

    "Update"

--- shell/syncweblog.sh
+++ shell/syncweblog.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Purely personal. rsyncs my local html files with my webserver :-)
 
-rsync -av /home/reinout/zelf/reinout.vanrees.org/docs/build/html/ vanrees.org:/srv/reinout.vanrees.org/var/www
+rsync -av ~/zelf/reinout.vanrees.org/docs/build/html/ vanrees.org:/srv/reinout.vanrees.org/var/www
