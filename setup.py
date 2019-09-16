1a3a14d2aa3c35b747455bc7a5b1087ed7ec54a6
Reinout van Rees <reinout@vanrees.org>
2019-09-16 14:08:32 +0200

    black

--- setup.py
+++ setup.py
@@ -1,54 +1,54 @@
 from setuptools import setup
 
-version = '0.1.dev0'
+version = "0.1.dev0"
 
 
-setup(name='tools',
-      version=version,
-      description="Tools and scripts for Reinout",
-      long_description='',
-      classifiers=[],
-      keywords='',
-      author='Reinout van Rees',
-      author_email='reinout@vanrees.org',
-      url='http://reinout.vanrees.org',
-      license='GPL',
-      packages=['tools'],
-      namespace_packages=[],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          # 'media-manager',
-          # ^^^ Only on osx at the moment,included in the buildout
-
-          # 'createcoverage',
-          # 'eolfixer',
-          # 'nensbuild',
-          # 'nenslint',
-          'coverage',
-          'future',
-          'nose',
-          'pyserial',
-          # 'readline',
-          'six',  # Trying to make it explicit
-          ],
-      entry_points={
-        'console_scripts': [
-            'log = tools.engineerlog:main',
-            'add_time = tools.add_time:main',
-            'copytoblog = tools.blog:copytoblog',
-            'new_sermon = tools.blog:new_sermon',
-            'fixthunderbird = tools.thunderbird:fix_thunderbird',
-            'gh = tools.github:main',
-            'jsonformatter = tools.jsonformatter:main',
-            'latestentries = tools.blog:list_todays_entries',
-            'makedocs = tools.blog:makedocs',
-            'sommen = tools.sommen:main',
+setup(
+    name="tools",
+    version=version,
+    description="Tools and scripts for Reinout",
+    long_description="",
+    classifiers=[],
+    keywords="",
+    author="Reinout van Rees",
+    author_email="reinout@vanrees.org",
+    url="http://reinout.vanrees.org",
+    license="GPL",
+    packages=["tools"],
+    namespace_packages=[],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        # 'media-manager',
+        # ^^^ Only on osx at the moment,included in the buildout
+        # 'createcoverage',
+        # 'eolfixer',
+        # 'nensbuild',
+        # 'nenslint',
+        "coverage",
+        "future",
+        "nose",
+        "pyserial",
+        # 'readline',
+        "six",  # Trying to make it explicit
+    ],
+    entry_points={
+        "console_scripts": [
+            "log = tools.engineerlog:main",
+            "add_time = tools.add_time:main",
+            "copytoblog = tools.blog:copytoblog",
+            "new_sermon = tools.blog:new_sermon",
+            "fixthunderbird = tools.thunderbird:fix_thunderbird",
+            "gh = tools.github:main",
+            "jsonformatter = tools.jsonformatter:main",
+            "latestentries = tools.blog:list_todays_entries",
+            "makedocs = tools.blog:makedocs",
+            "sommen = tools.sommen:main",
             # 'tl = tools.timelog:add_timelog_entry',
             # 'pt = tools.timelog:main',
-            'gac = tools.git:main',
-            'mkinit = tools.mkinit:main',
-            'python_coding_cleanup = tools.python_coding_cleanup:main',
-            ],
-        },
-      )
+            "gac = tools.git:main",
+            "mkinit = tools.mkinit:main",
+            "python_coding_cleanup = tools.python_coding_cleanup:main",
+        ]
+    },
+)
