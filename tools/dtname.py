1a6f1778deaea1c60d17cb1ab40204128ebbad2a
Reinout van Rees <reinout@vanrees.org>
2022-11-29 12:09:37 +0100

    regex fix

--- tools/dtname.py
+++ tools/dtname.py
@@ -21,7 +21,7 @@ def slugify(value):
     value = (
         unicodedata.normalize("NFKD", value).encode("ascii", "ignore").decode("ascii")
     )
-    value = re.sub(r"[^\w\s-]", "", value.lower())
+    value = re.sub(r"[^\w\s\-]", "", value.lower())
     return re.sub(r"[-\s]+", "-", value).strip("-_")
 
 
